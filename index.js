0828c0b537cb512f42839d837282ead91714a61a
alexey Konstantinov <applejack66@yandex.ru>
2022-09-15 23:33:03 +0300

    final

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './src/index.css';
+import App from './src/App';
+import reportWebVitals from './src/reportWebVitals';
 import {DAppProvider, Goerli, Mainnet} from "@usedapp/core";
 import { getDefaultProvider } from 'ethers'
 
