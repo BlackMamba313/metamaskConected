8199bbc15f9b5bb64542cfb1fc2919e5ee178c01
alexey Konstantinov <applejack66@yandex.ru>
2022-09-15 23:14:57 +0300

    final

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {Grid,} from "@mui/material";
-import ButtonConnect from "../components/ButtonConnect";
-import AccountPopUp from "../components/AccountPopUp";
+import ButtonConnect from "./components/ButtonConnect";
+import AccountPopUp from "./components/AccountPopUp";
 
 function App() {
     const [open, setOpen] = React.useState(false);
