6f53601fb4ad3e3b53df1d6a217b8bec52234641
alexey Konstantinov <applejack66@yandex.ru>
2022-09-16 12:43:04 +0300

    final

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {DAppProvider, Goerli, Mainnet} from "@usedapp/core";
@@ -18,11 +17,12 @@ const config = {
 ReactDOM.render(
   <React.StrictMode>
       <DAppProvider config={ config }>
+          <div>sdfasdfasdfasdfasdfasdfasd</div>
          <App />
       </DAppProvider>
   </React.StrictMode>,
     document.getElementById('root')
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
